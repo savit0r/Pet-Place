7ae21cb2af1ee7b5fe23508ab30c66ccb6afba10
Rohit <rohitsatre33@gmail.com>
2024-11-14 11:47:26 +0530

    Intro section added

--- main.js
+++ main.js
@@ -14,3 +14,39 @@ navLinks.addEventListener("ckick", (e) =>{
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const ScrollRevealOption ={
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__content h4", {
+    ...ScrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...ScrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content h2", {
+    ...ScrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__content p", {
+    ...ScrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".header__btn", {
+    ...ScrollRevealOption,
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".intro__card", {
+    ...ScrollRevealOption,
+    interval: 500,
+});
+
